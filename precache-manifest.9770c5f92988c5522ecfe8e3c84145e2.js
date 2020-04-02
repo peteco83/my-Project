f7e11b02487918bc5a42b1d8f529ba4a329b5c61
peteco83 <peteco1983@outlook.com>
2020-04-02 12:38:14 +0200

    Updates

---
+++ precache-manifest.9770c5f92988c5522ecfe8e3c84145e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de65c527b49d39e06c3e95e7c85d80e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22c90ca4097780e61bea",
+    "url": "./static/css/main.b1693eb1.chunk.css"
+  },
+  {
+    "revision": "a5b89a4d58dfc59365ed",
+    "url": "./static/js/2.1f362c4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1f362c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c90ca4097780e61bea",
+    "url": "./static/js/main.dad66d38.chunk.js"
+  },
+  {
+    "revision": "994fcb3a74b4aaef6750",
+    "url": "./static/js/runtime-main.107712e5.js"
+  }
+]);
